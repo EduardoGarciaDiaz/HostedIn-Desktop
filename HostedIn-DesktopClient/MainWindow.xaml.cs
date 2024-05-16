4b54805efadd30ca770b4fc8cc82ddbc14111d22
TristanSantiago1 <tristan.santiago520@gmail.com>
2024-05-16 16:30:48 -0600

    .

--- HostedIn-DesktopClient/MainWindow.xaml.cs
+++ HostedIn-DesktopClient/MainWindow.xaml.cs
@@ -40,14 +40,19 @@ namespace HostedIn_DesktopClient
         private async void  CreateNewAccount()
         {
             //TO-DO recuperar campos de GUI
-            User user = new User("correoPrueba@gmail.com", "Tristan Eduardo", DateTime.Now, "2281549222", "contraseña", "estudiante", "Xalapa", new byte[0]);
+            User user = new User("correoPrueba0@gmail.com", "Tristan Eduardo", DateTime.Now, "2255549222", "contraseña", "estudiante", "Xalapa", new byte[0]);
             ApiRest apiRest = ApiRest.GetInstance();
             HttpResponseMessage result = await apiRest.CreateAccount(user);
+            string token ="";
+            if (result.Headers.TryGetValues("Authorization", out IEnumerable<string> values))
+            {
+                token = values.FirstOrDefault().Substring(7);
+            }
             string jsonResponse = await result.Content.ReadAsStringAsync();
             if (result.IsSuccessStatusCode)
             {
                 User newSaved = JsonConvert.DeserializeObject<User>(jsonResponse);
-                txbBlock.Text = "El usaurio " + newSaved.fullName + " fue guardado correctamente";
+                txbBlock.Text = "El usaurio " + newSaved.fullName + " fue guardado correctamente, con el token " + token;
             }
             else
             {
